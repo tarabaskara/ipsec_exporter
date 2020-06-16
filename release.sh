42bd6a35bc9df1e34df95936bad795f5cdf376c9
Beni Budiharto <gnu.iben@gmail.com>
2020-06-17 04:46:26 +0700

    add release.sh

---
+++ release.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+VERSION=$1
+
+GOOS=linux GOARCH=amd64 go build .
+cp ipsec_exporter "ipsec_exporter-$VERSION.linux-amd64"
+rm -rf "ipsec_exporter-$VERSION.linux-amd64.tar.gz"
+tar czfv "ipsec_exporter-$VERSION.linux-amd64.tar.gz" "ipsec_exporter-$VERSION.linux-amd64"
+sha256sum "ipsec_exporter-$VERSION.linux-amd64.tar.gz" | tee sha256sums.txt
