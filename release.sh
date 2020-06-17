920cf83a367cb7eb4b784e14a07bab27e5721adf
Beni Budiharto <gnu.iben@gmail.com>
2020-06-17 10:58:45 +0700

    feat: add release generator bin

--- release.sh
+++ release.sh
@@ -2,7 +2,6 @@
 VERSION=$1
 
 GOOS=linux GOARCH=amd64 go build .
-cp ipsec_exporter "ipsec_exporter-$VERSION.linux-amd64"
 rm -rf "ipsec_exporter-$VERSION.linux-amd64.tar.gz"
-tar czfv "ipsec_exporter-$VERSION.linux-amd64.tar.gz" "ipsec_exporter-$VERSION.linux-amd64"
+tar czfv "ipsec_exporter-$VERSION.linux-amd64.tar.gz" "ipsec_exporter"
 sha256sum "ipsec_exporter-$VERSION.linux-amd64.tar.gz" | tee sha256sums.txt
